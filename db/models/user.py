09d5bf38e39dc95f6a2c1e6b8b5ecea0275e119b
Jenil Savani <jenilsavani1@gmail.com>
2025-09-26 22:29:09 +0530

    feat(db): add GUID column to users table for unique identification

--- db/models/user.py
+++ db/models/user.py
@@ -1,11 +1,13 @@
+import uuid
 from sqlalchemy import Column, Integer, String, BigInteger, Text
-from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
 
 from db.index import Base
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    guid = Column(UUID(as_uuid=True), unique=True, nullable=False, default=uuid.uuid4)
     email = Column(String(255), unique=True, nullable=False)
     username = Column(String(50), unique=True, nullable=True)
     password_hash = Column(Text, nullable=False)
