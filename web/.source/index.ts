d7d905c4709fd688b7835818d61f06c1b7ee5c89
Jenil Savani <jenilsavani1@gmail.com>
2025-10-06 22:18:19 +0530

    Remove source folder from tracking

--- web/.source/index.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck -- skip type checking
-import * as docs_2 from "../content/docs/dir/page.mdx?collection=docs&hash=1759768071066"
-import * as docs_1 from "../content/docs/index.mdx?collection=docs&hash=1759768071066"
-import * as docs_0 from "../content/docs/getting-started.mdx?collection=docs&hash=1759768071066"
-import { _runtime } from "fumadocs-mdx/runtime/next"
-import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"getting-started.mdx","fullPath":"content/docs/getting-started.mdx"}, data: docs_0 }, { info: {"path":"index.mdx","fullPath":"content/docs/index.mdx"}, data: docs_1 }, { info: {"path":"dir/page.mdx","fullPath":"content/docs/dir/page.mdx"}, data: docs_2 }], [])
