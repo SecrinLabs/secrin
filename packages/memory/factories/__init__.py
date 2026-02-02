3ad64aa36977259820a9b8a0cd0a705baba734d4
Jenil Savani <jenilsavani1@gmail.com>
2026-02-03 00:13:24 +0530

    fix: removed the not needed code

--- packages/memory/factories/__init__.py
+++
@@ -1,8 +0,0 @@
-from packages.memory.factories.embedding_factory import EmbeddingStrategyFactory
-from packages.memory.factories.llm_factory import LLMProviderFactory, create_llm_provider
-
-__all__ = [
-    "EmbeddingStrategyFactory",
-    "LLMProviderFactory",
-    "create_llm_provider"
-]
--- packages/memory/llm/providers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-LLM providers package.
-Exports all available provider implementations.
-"""
-
-from packages.memory.llm.providers.ollama import OllamaProvider
-
-__all__ = ["OllamaProvider"]
